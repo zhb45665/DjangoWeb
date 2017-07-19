0d949d6c4a21ad4b16e732c5857932771214d3da
zhangbiao <zhb45665@126.com>
2017-07-19 22:54:25 +0800

    修改后台显示

--- DjangoWeb/app/models.py
+++ DjangoWeb/app/models.py
@@ -9,9 +9,11 @@ class BlogPost(models.Model):
     title = models.CharField(max_length = 150)#标题
     body = models.TextField()#正文
     timestamp = models.DateTimeField()#时间
-    def __unicode__(self):
+    def __str__(self):
         return self.title
     class Meta():
+        verbose_name = '文章'
+        verbose_name_plural = '文章'
         ordering = ['-timestamp']
     def get_absolute_url(self):
         return self.body
